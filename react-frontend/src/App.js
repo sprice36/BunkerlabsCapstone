722905153f07d6676aaa86c33ad7afbd9f632147
Dan McKinney <3dhdaniel@gmail.com>
2018-08-06 16:25:27 -0400

    adding forms page as well as edit page

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
+import {
+  BrowserRouter as Router,
+  Route, 
+  Switch
+} from 'react-router-dom';
+import BunkerForms from './BunkerForms';
+let App = () => 
+  <Router>
+    <div className='app'>
+      <Switch>
+        <Route path='/' component={ BunkerForms } />
+        </Switch>
+        </div>
+        </Router>
+      
 export default App;
